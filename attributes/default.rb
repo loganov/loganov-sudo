891ba13b11ab867f9437833a37050d61c49a1231
Gregory Weaver <gregory.weaver@gmail.com>
2014-10-17 13:39:52 -0700

    Initial commit.

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['authorization']['sudo']['groups']            = ['sysadmin', 'wheel']
+default['authorization']['sudo']['passwordless']      = true
+default['authorization']['sudo']['include_sudoers_d'] = true
---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: loganov-sudo
+# Recipe:: default
+#
+# Copyright 2014, Loganov Industries
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'sudo'
